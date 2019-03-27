34bb65a758ae69802dc7a43847fa46263712e28a
Ontheroad <1465371751@qq.com>
2019-03-27 20:36:41 +0800

    回文数的判断

---
+++ src/com/Solution/String/isPalindrome_9.java
@@ -0,0 +1,31 @@
+package com.Solution.String;
+
+/**
+ * 题目：判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 思路：由于要求不能开辟新的空间，不能转换为字符串，所以就要从数字本身入手。
+ * 将数字分为两部分，后一部分进行翻转判断两数是否相同。
+ */
+public class isPalindrome_9 {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        if(x!=0&&x%10==0)
+            return false;
+        int right=0;//将数的右半部分进行翻转
+        while(x>right)
+        {
+            right=right*10+x%10;
+            x=x/10;
+        }
+        return x==right||x==right/10;//数字位数为偶数或者奇数的情况进行相等判断
+    }
+
+    public static void main(String[] args) {
+        boolean ans1=new isPalindrome_9().isPalindrome(-11);
+        boolean ans2=new isPalindrome_9().isPalindrome(11);
+        boolean ans3=new isPalindrome_9().isPalindrome(121);
+        boolean ans4=new isPalindrome_9().isPalindrome(0);
+        System.out.println(ans1+" "+ans2+" "+ans3+" "+ans4);
+    }
+}
+
