74e963bc9239b8c72d8e1f291c1ea48bd308841e
Ontheroad <1465371751@qq.com>
2019-04-29 17:32:16 +0800

    455_分发饼干

---
+++ src/com/Solution/Greedy/findContentChildren_455_分发饼干.java
@@ -0,0 +1,57 @@
+package com.Solution.Greedy;
+
+import java.util.Arrays;
+
+/**
+ * 题目：假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j ，都有一个尺寸 sj 。如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ * 注意：
+ * 你可以假设胃口值为正。
+ * 一个小朋友最多只能拥有一块饼干。
+ *
+ * 思路：贪心算法
+ */
+public class findContentChildren_455_分发饼干 {
+    //首先想到的思路
+//    public int findContentChildren(int[] g, int[] s) {
+//        boolean res[]=new boolean[g.length];
+//        Arrays.sort(g);
+//        Arrays.sort(s);
+//        int count=0;
+//        for (int i = 0; i < s.length; i++) {
+//            for (int j = 0; j < g.length; j++) {
+//                if(s[i]>=g[j]&&res[j]==false)
+//                {
+//                    res[j]=true;
+//                    count++;
+//                    break;
+//                }
+//                if(s[i]<g[j])
+//                    break;
+//            }
+//        }
+//        return count;
+//    }
+
+    //优化思路
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi=0;
+        int si=0;
+        while(gi<g.length&&si<s.length)
+        {
+            if(g[gi]<=s[si]){
+                gi++;
+            }
+            si++;
+        }
+        return gi;
+    }
+
+    public static void main(String[] args) {
+        int g[]={1,2,3,4,5,6};
+        int s[]={1,2,3,3,8};
+        int ans=new findContentChildren_455_分发饼干().findContentChildren(g,s);
+        System.out.println(ans);
+    }
+}
