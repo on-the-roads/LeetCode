483c0829b5a248ef508293e3969aed81d8a99ffb
Ontheroad <1465371751@qq.com>
2019-05-07 23:09:04 +0800

    392_判断子序列

---
+++ src/com/Solution/Greedy/isSubsequence_392_判断子序列.java
@@ -0,0 +1,35 @@
+package com.Solution.Greedy;
+
+/**
+ * 题目：给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ *
+ * 思路：贪心算法，根据s中的每个字符，去寻找t中是否有满足条件的字符存在。
+ */
+public class isSubsequence_392_判断子序列 {
+    public boolean isSubsequence(String s, String t) {
+        int index=-1;
+        for (int i = 0; i < s.length(); i++) {//首先遍历s中的字符
+            int j=index+1;
+            for(;j<t.length();j++)//对每个s的字符，从t中寻找是否有满足条件的字符存在
+            {
+                if(t.charAt(j)==s.charAt(i))//找到字符，记录当前的下标位置
+                {
+                    index=j;
+                    break;
+                }
+                else if(j==t.length()-1)//没找到字符，则直接返回false
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s="abc";
+        String t="aahbgdc";
+        boolean ans=new isSubsequence_392_判断子序列().isSubsequence(s,t);
+        System.out.println(ans);
+    }
+}
