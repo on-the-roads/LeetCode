fbb8f2abe000d78e8370d520f3274ea58c248733
Ontheroad <1465371751@qq.com>
2019-05-09 17:20:42 +0800

    392_判断子序列

--- src/com/Solution/Greedy/isSubsequence_392_判断子序列.java
+++ src/com/Solution/Greedy/isSubsequence_392_判断子序列.java
@@ -4,32 +4,51 @@ package com.Solution.Greedy;
  * 题目：给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
  * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
  * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
- *
+ * <p>
  * 思路：贪心算法，根据s中的每个字符，去寻找t中是否有满足条件的字符存在。
  */
 public class isSubsequence_392_判断子序列 {
+    //做法1
+//    public boolean isSubsequence(String s, String t) {
+//        if(s==null||s.length()==0)
+//            return true;
+//        if(t==null||t.length()==0)
+//            return false;
+//        int index=-1;
+//        for (int i = 0; i < s.length(); i++) {//首先遍历s中的字符
+//            int j=index+1;
+//            for(;j<t.length();j++)//对每个s的字符，从t中寻找是否有满足条件的字符存在
+//            {
+//                if(t.charAt(j)==s.charAt(i))//找到字符，记录当前的下标位置
+//                {
+//                    index=j;
+//                    break;
+//                }
+//                else if(j==t.length()-1)//没找到字符，则直接返回false
+//                    return false;
+//            }
+//            if(j==t.length())//s中的字符还没有找完就已经遍历完t了
+//                return false;
+//        }
+//        return true;
+//    }
+
+
+    //做法2：对方法1的优化,利用api方法indexOf定位元素
     public boolean isSubsequence(String s, String t) {
-        int index=-1;
-        for (int i = 0; i < s.length(); i++) {//首先遍历s中的字符
-            int j=index+1;
-            for(;j<t.length();j++)//对每个s的字符，从t中寻找是否有满足条件的字符存在
-            {
-                if(t.charAt(j)==s.charAt(i))//找到字符，记录当前的下标位置
-                {
-                    index=j;
-                    break;
-                }
-                else if(j==t.length()-1)//没找到字符，则直接返回false
-                    return false;
-            }
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index+1);
+            if (index == -1)
+                return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String s="abc";
-        String t="aahbgdc";
-        boolean ans=new isSubsequence_392_判断子序列().isSubsequence(s,t);
+        String s = "abc";
+        String t = "aahbgdc";
+        boolean ans = new isSubsequence_392_判断子序列().isSubsequence(s, t);
         System.out.println(ans);
     }
 }
---
+++ src/com/Solution/dynamic_programming/minSteps_650_只有两个键的键盘.java
@@ -0,0 +1,29 @@
+package com.Solution.dynamic_programming;
+
+/**
+ * 题目：最初在一个记事本上只有一个字符 'A'。你每次可以对这个记事本进行两种操作：
+ * Copy All (复制全部) : 你可以复制这个记事本中的所有字符(部分的复制是不允许的)。
+ * Paste (粘贴) : 你可以粘贴你上一次复制的字符。
+ * 给定一个数字 n 。你需要使用最少的操作次数，在记事本中打印出恰好 n 个 'A'。输出能够打印出 n 个 'A' 的最少操作次数。
+ *
+ * 思路：动态规划，尝试写出几个数字用不完全归纳法就可以找到规律，对于质数，结果只能是质数；对于非质数，判断是否有因子2和3.
+ */
+public class minSteps_650_只有两个键的键盘 {
+    public int minSteps(int n) {
+        int[] dp=new int[n+1];
+        for (int i = 2; i <=n ; i++) {
+            if(i%2==0)
+                dp[i]=dp[i/2]+2;
+            else if(i%3==0)
+                dp[i]=dp[i/3]+3;
+            else//质数
+                dp[i]=i;
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int ans=new minSteps_650_只有两个键的键盘().minSteps(16);
+        System.out.println(ans);
+    }
+}
