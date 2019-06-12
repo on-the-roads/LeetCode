59498f0a76a80fd2972ae6bf24d85eeb7b984430
Ontheroad <1465371751@qq.com>
2019-06-12 20:35:42 +0800

    415_字符串相加

---
+++ src/com/Solution/Math/addStrings_415_字符串相加.java
@@ -0,0 +1,35 @@
+package com.Solution.Math;
+
+/**
+ * 题目：给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+ * 注意：
+ * num1 和num2 的长度都小于 5100.
+ * num1 和num2 都只包含数字 0-9.
+ * num1 和num2 都不包含任何前导零。
+ * 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
+ *
+ * 思路：同问题二进制求和
+ */
+public class addStrings_415_字符串相加 {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb=new StringBuilder();
+        int carry=0;
+        int index1=num1.length()-1;
+        int index2=num2.length()-1;
+        while (carry>0||index1>=0||index2>=0)
+        {
+            if(index1>=0)
+                carry+=num1.charAt(index1--)-'0';
+            if(index2>=0)
+                carry+=num2.charAt(index2--)-'0';
+            sb.append(carry%10);
+            carry=carry/10;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String ans=new addStrings_415_字符串相加().addStrings("99","199");
+        System.out.println(ans);
+    }
+}
